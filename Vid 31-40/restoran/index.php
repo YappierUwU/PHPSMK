9322c3e8e3dfaae27e6c0672ccffa4dcca2f1816
Yappier Albertus <87288148+YappierUwU@users.noreply.github.com>
2021-10-10 15:46:01 +0700

    Add files via upload

---
+++ Vid 31-40/restoran/index.php
@@ -0,0 +1,88 @@
+<?php
+
+session_start();
+require_once "dbcontroller.php";
+$db = new DB;
+
+$sql = "SELECT * FROM tblkategori ORDER BY kategori";
+$row = $db->getALL($sql);
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restoran UWU | Aplikasi Restoran SMK</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-3">
+                <h2><a href="index.php"> Restoran UWU </a></h2>
+            </div>
+            <div class="col-md-9">
+                <div class="float-end mt-4 me-4"> Logout </div>
+                <div class="float-end mt-4 me-4"> Login </div>
+                <div class="float-end me-4 mt-4">Pelanggan</div>
+                <div class="float-end me-4 mt-4">Daftar</div>
+
+            </div>
+
+        </div>
+
+        <div class="row mt-3">
+            <div class="col-md-3">
+                <h3>Kategori</h3>
+                <hr>
+                <?php if (!empty($row)) { ?>
+                    <ul class="nav flex-column">
+                        <?php foreach ($row as $r) : {
+                            } ?>
+                            <li class="nav-item"><a class="nav-link" href="?f=home&m=produk&id=<?php echo $r['idkategori'] ?>"><?php echo $r['kategori'] ?></a></li>
+                        <?php endforeach ?>
+                    </ul>
+                <?php } ?>
+            </div>
+
+            <div class="col-md-9">
+                <?php
+
+                if (isset($_GET['f']) && isset($_GET['m'])) {
+                    $f = $_GET['f'];
+                    $m = $_GET['m'];
+
+                    $file = $f . '/' . $m . '.php';
+                    require_once $file;
+                } else {
+                    require_once "home/produk.php";
+                }
+
+
+
+                ?>
+            </div>
+        </div>
+
+
+        <div class="row mt-5">
+            <div class="col">
+                <p class="text-center"> 2021 - copyright@yappier.albert1.com</p>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
